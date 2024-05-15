6f68884ef6fb33779eaf09d55843bd0a0932dad3
TerezKo <119680300+TerezKo@users.noreply.github.com>
2024-05-15 09:49:47 +0200

    Add files via upload

---
+++ schema.sql
@@ -0,0 +1,76 @@
+-- schema.sql
+
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS non_prefer;
+DROP TABLE IF EXISTS prefer;
+DROP TABLE IF EXISTS submissions;
+DROP TABLE IF EXISTS schedules;
+DROP TABLE IF EXISTS lekari;
+
+CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    first_name TEXT,
+    last_name TEXT,
+    email TEXT UNIQUE,
+    password TEXT,
+    max_hours TEXT,
+    min_hours TEXT,
+    position TEXT,
+    is_certified TEXT,
+    address TEXT,
+    mobile TEXT,
+    birthdate DATE,
+    workplace TEXT,
+    work_at_night TEXT,
+    status TEXT
+);
+
+
+CREATE TABLE IF NOT EXISTS non_prefer (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER,
+    day INTEGER,
+    shift_type TEXT,
+    reason_strength TEXT,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+CREATE TABLE IF NOT EXISTS prefer (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER,
+    day INTEGER,
+    shift_type TEXT,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+CREATE TABLE IF NOT EXISTS submissions (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        user_id INTEGER NOT NULL,
+        submission_date TIMESTAMP NOT NULL,
+        FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+CREATE TABLE IF NOT EXISTS schedules (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        month_year INTEGER UNIQUE NOT NULL,
+        lekari TEXT,
+        sestry_JIS TEXT,
+        sestry_odd TEXT,
+        prak_sestry_JIS TEXT,
+        prak_sestry_odd TEXT,
+        oset_JIS TEXT,
+        oset_odd TEXT
+);
+
+CREATE TABLE IF NOT EXISTS lekari (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        pracovisko TEXT,
+        pondelok TEXT,
+        utorok TEXT,
+        streda TEXT,
+        stvrtok TEXT,
+        piatok TEXT
+);
+
+
+
